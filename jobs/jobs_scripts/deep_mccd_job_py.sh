b2bc3516d3280e5ec43bd39fcc347b4015a7f95f
Tobias Liaudat <tobiasliaudat@gmail.com>
2022-02-09 18:59:17 +0100

    Fixing scripts

---
+++ jobs/jobs_scripts/deep_mccd_job_py.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+##########################
+# SMP Script for CANDIDE #
+##########################
+# Receive email when job finishes or aborts
+#PBS -M tobias.liaudat@cea.fr
+#PBS -m ea
+# Set a name for the job
+#PBS -N shapepipe_job
+# Join output and errors in one file
+#PBS -j oe
+# Set maximum computing time (e.g. 5min)
+#PBS -l walltime=90:00:00
+# Request number of cores
+#PBS -l nodes=n16:ppn=2:hasgpu
+
+# Full path to environment
+export SPENV="$HOME/.conda/envs/shapepipe"
+export CONFDIR="$HOME/github/aziz_repos/deep_mccd/jobs"
+
+# Activate conda environment
+module load tensorflow/2.4
+module load intel/19.0/2
+source activate shapepipe
+
+# Run ShapePipe using full paths to executables
+python $SPENV/bin/shapepipe_run.py -c $CONFDIR/config_files/deep_mccd.ini
+
+# Return exit code
+exit 0
