268b8a12afc2b4bc4b97eeaaef2b26250e870e77
Tobias Liaudat <tobiasliaudat@gmail.com>
2022-02-11 15:55:57 +0100

    change of load module

--- jobs/jobs_scripts/dataset_gen_job.sh
+++ jobs/jobs_scripts/dataset_gen_job.sh
@@ -7,7 +7,7 @@
 #PBS -M tobias.liaudat@cea.fr
 #PBS -m ea
 # Set a name for the job
-#PBS -N shapepipe_dataset_gen
+#PBS -N dataset_gen
 # Join output and errors in one file
 #PBS -j oe
 # Set maximum computing time (e.g. 5min)
@@ -16,11 +16,12 @@
 #PBS -l nodes=n02:ppn=32
 
 # Full path to environment
-export SPENV="$HOME/.conda/envs/shapepipe"
+export SPENV="$HOME/.conda/envs/shapepipe_mccd"
 export CONFDIR="$HOME/github/aziz_repos/deep_mccd"
 
 # Activate conda environment
-module load tensorflow/2.4
+# module load tensorflow/2.4
+module load intelpython/3-2020.1
 module load intel/19.0/2
 source activate shapepipe_mccd
 
