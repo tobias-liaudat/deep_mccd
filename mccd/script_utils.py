f7bac3d7fdc500c10ef181a1d4b382858d352215
Tobias Liaudat <tobiasliaudat@gmail.com>
2022-02-22 14:23:18 +0100

    Comment GPU allocation growth

--- mccd/script_utils.py
+++ mccd/script_utils.py
@@ -11,11 +11,11 @@ from mccd.denoising.unets.unet import Unet
 from mccd.denoising.evaluate import keras_psnr, center_keras_psnr
 from mccd.denoising.preprocessing import eigenPSF_data_gen
 
-from tensorflow.compat.v1 import ConfigProto
-from tensorflow.compat.v1 import InteractiveSession
-config = ConfigProto()
-config.gpu_options.allow_growth = True
-session = InteractiveSession(config=config)
+# from tensorflow.compat.v1 import ConfigProto
+# from tensorflow.compat.v1 import InteractiveSession
+# config = ConfigProto()
+# config.gpu_options.allow_growth = True
+# session = InteractiveSession(config=config)
 
 
 def train_learnlets(**args):
