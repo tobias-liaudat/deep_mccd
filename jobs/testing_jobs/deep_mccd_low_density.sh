4af480f969c0feeb35744f91cfa1bd9a7f3146b8
Tobias Liaudat <tobiasliaudat@gmail.com>
2022-02-24 19:35:00 +0100

    Add jobs for low/mid/high star density testing

---
+++ jobs/testing_jobs/deep_mccd_low_density.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+##########################
+# SMP Script for CANDIDE #
+##########################
+# Receive email when job finishes or aborts
+#PBS -M tobias.liaudat@cea.fr
+#PBS -m ea
+# Set a name for the job
+#PBS -N low_den_deep_mccd
+# Join output and errors in one file
+#PBS -j oe
+# Set maximum computing time (e.g. 5min)
+#PBS -l walltime=99:00:00
+# Request number of cores
+#PBS -l nodes=n03:ppn=4:hasgpu
+
+# Full path to environment
+export SPENV="$HOME/.conda/envs/shapepipe"
+export CONFDIR="$HOME/github/aziz_repos/deep_mccd"
+
+# Activate conda environment
+module load tensorflow/2.4
+module load intel/19.0/2
+source activate shapepipe
+
+# Run ShapePipe using full paths to executables
+/home/tliaudat/.local/bin/shapepipe_run.py -c $CONFDIR/jobs/testing_jobs/configs/pipe_deep_mccd_low_den_L256.ini
+/home/tliaudat/.local/bin/shapepipe_run.py -c $CONFDIR/jobs/testing_jobs/configs/pipe_deep_mccd_low_den_L512.ini
+/home/tliaudat/.local/bin/shapepipe_run.py -c $CONFDIR/jobs/testing_jobs/configs/pipe_deep_mccd_low_den_U32.ini
+
+# Return exit code
+exit 0
