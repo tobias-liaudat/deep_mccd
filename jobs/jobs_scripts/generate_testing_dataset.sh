5a84c0669067d0ac5ecc4f389b9666d4e748b14c
Tobias Liaudat <tobiasliaudat@gmail.com>
2022-02-24 16:35:11 +0100

    Add job to generate testing dataset

---
+++ jobs/jobs_scripts/generate_testing_dataset.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+##########################
+# SMP Script for CANDIDE #
+##########################
+# Receive email when job finishes or aborts
+#PBS -M tobias.liaudat@cea.fr
+#PBS -m ea
+# Set a name for the job
+#PBS -N testing_dataset_gen
+# Join output and errors in one file
+#PBS -j oe
+# Set maximum computing time (e.g. 5min)
+#PBS -l walltime=90:00:00
+# Request number of cores
+#PBS -l nodes=1:ppn=3
+
+# Full path to environment
+export SPENV="$HOME/.conda/envs/shapepipe_mccd"
+export CONFDIR="$HOME/github/aziz_repos/deep_mccd"
+
+# Activate conda environment
+module purge
+# module load tensorflow/2.4
+module load intelpython/3-2020.1
+module load intel/19.0/2
+source activate shapepipe_mccd
+
+# Run ShapePipe using full paths to executables
+python $CONFDIR/scripts/generate_testing_dataset.py
+
+# Return exit code
+exit 0
