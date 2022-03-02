5ca79bac99e0a07aa374b5d5664acdc871e37ee7
Tobias Liaudat <tobiasliaudat@gmail.com>
2022-03-02 16:26:46 +0100

    Adding new jobs and configs

--- jobs/testing_jobs/testing_jobs/test_deep_mccd_low_real.sh
+++ jobs/testing_jobs/testing_jobs/test_deep_mccd_low_real.sh
@@ -7,26 +7,26 @@
 #PBS -M tobias.liaudat@cea.fr
 #PBS -m ea
 # Set a name for the job
-#PBS -N test_new_wav_mccd
+#PBS -N test_low_den_deep_mccd
 # Join output and errors in one file
 #PBS -j oe
 # Set maximum computing time (e.g. 5min)
 #PBS -l walltime=95:00:00
 # Request number of cores
-#PBS -l nodes=n05:ppn=3
+#PBS -l nodes=n16:ppn=3:hasgpu
 
 # Full path to environment
 export SPENV="$HOME/.conda/envs/shapepipe"
 export CONFDIR="$HOME/github/aziz_repos/deep_mccd"
 
 # Activate conda environment
-module purge
 module load tensorflow/2.4
 module load intel/19.0/2
 source activate shapepipe
 
 # Run ShapePipe using full paths to executables
-/home/tliaudat/.local/bin/shapepipe_run.py -c $CONFDIR/jobs/testing_jobs/testing_configs/test_pipe_classic_mccd_new_wav.ini
+# Flat SNR
+/home/tliaudat/.local/bin/shapepipe_run.py -c $CONFDIR/jobs/testing_jobs/testing_configs/test_pipe_deep_mccd_low_den_L256.ini
 
 
 # Return exit code
--- jobs/testing_jobs/testing_jobs/test_mccd_classic_low_real.sh
+++ jobs/testing_jobs/testing_jobs/test_mccd_classic_low_real.sh
@@ -7,27 +7,27 @@
 #PBS -M tobias.liaudat@cea.fr
 #PBS -m ea
 # Set a name for the job
-#PBS -N test_new_wav_mccd
+#PBS -N test_classic_mccd
 # Join output and errors in one file
 #PBS -j oe
 # Set maximum computing time (e.g. 5min)
-#PBS -l walltime=95:00:00
+#PBS -l walltime=25:00:00
 # Request number of cores
-#PBS -l nodes=n05:ppn=3
+#PBS -l nodes=n02:ppn=3
 
 # Full path to environment
-export SPENV="$HOME/.conda/envs/shapepipe"
+export SPENV="$HOME/.conda/envs/shapepipe_mccd"
 export CONFDIR="$HOME/github/aziz_repos/deep_mccd"
 
 # Activate conda environment
 module purge
-module load tensorflow/2.4
+module load intelpython/3-2020.1
 module load intel/19.0/2
-source activate shapepipe
+source activate shapepipe_mccd
 
 # Run ShapePipe using full paths to executables
-/home/tliaudat/.local/bin/shapepipe_run.py -c $CONFDIR/jobs/testing_jobs/testing_configs/test_pipe_classic_mccd_new_wav.ini
-
+# /home/tliaudat/.local/bin/shapepipe_run.py -c $CONFDIR/config_files/mccd_eigen_generation.ini
+$SPENV/bin/shapepipe_run -c $CONFDIR/jobs/testing_jobs/testing_configs/test_pipe_classic_mccd_real_wav3.ini
 
 # Return exit code
 exit 0
